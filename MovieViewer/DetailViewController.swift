18fb491f402d4989d4579ee81d96010529cbbca6
Patwardhan, Saurabh <Saurabh.Patwardhan@capitalone.com>
2016-10-14 22:09:03 -0700

    Commit to git

--- MovieViewer/DetailViewController.swift
+++ MovieViewer/DetailViewController.swift
@@ -9,9 +9,40 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var infoView: UIView!
+   
+    
+    var movie : NSDictionary!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: infoView.frame.origin.y + infoView.frame.size.height)
+        
+        let title = movie["title"] as? String
+        let overview = movie["overview"] as? String
+        
+        titleLabel.text = title
+        overviewLabel.text = overview
+        
+        overviewLabel.sizeToFit()
+        
+        if let posterPath = movie["poster_path"] as? String {
+            let posterBaseUrl = "http://image.tmdb.org/t/p/w500"
+            let posterUrl = URL(string: posterBaseUrl + posterPath)
+            posterImageView.setImageWith(posterUrl!)
+        }
+        else {
+            // No poster image. Can either set to nil (no image) or a default movie poster image
+            // that you include as an asset
+            posterImageView.image = nil
+        }
+
 
         // Do any additional setup after loading the view.
     }
--- MovieViewer/NowPlayingViewController.swift
+++ MovieViewer/NowPlayingViewController.swift
@@ -7,11 +7,23 @@
 //
 
 import UIKit
+import AFNetworking
 
-class NowPlayingViewController: UIViewController {
+class NowPlayingViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies : [NSDictionary]?
+    
+    var endpoint : String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        networkRequest()
 
         // Do any additional setup after loading the view.
     }
@@ -21,15 +33,80 @@ class NowPlayingViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let movies = movies {
+            return movies.count
+        }else {
+            return 0
+        }
+    }
+    
+    func networkRequest(){
+        print ("endpoint: " , endpoint)
+        let apiKey = "2bc27eecd1ba89ce134f4ff3d131d126"
+        let urlString = "https://api.themoviedb.org/3/movie/\(self.endpoint!)?api_key=\(apiKey)"
+        let url = URL(string : urlString)
+        let request = URLRequest(url: url!)
+        let session = URLSession(configuration : URLSessionConfiguration.default,delegate: nil,delegateQueue: OperationQueue.main)
+        
+        
+        
+        let task : URLSessionDataTask = session.dataTask(with: request,completionHandler: {(dataOrNil, response, error)  in
+            if let data = dataOrNil {
+                if let responseDictionary = try!
+                    JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary{NSLog("responses \(responseDictionary)")
+                    
+                    self.movies = responseDictionary["results"] as? [NSDictionary]
+                    self.tableView.reloadData()
+                }
+            }
+            
+        });
+        task.resume()
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NowPlayingCell", for: indexPath) as! NowPlayingViewCell
+        let movie = movies?[indexPath.row]
+        let title = movie?["title"] as! String
+        let overview = movie?["overview"] as! String
+        
+        if let posterPath = movie?["poster_path"] as? String {
+            let posterBaseUrl = "http://image.tmdb.org/t/p/w500"
+            let posterUrl = URL(string: posterBaseUrl + posterPath)
+            cell.posterView.setImageWith(posterUrl!)
+        }
+        else {
+            // No poster image. Can either set to nil (no image) or a default movie poster image
+            // that you include as an asset
+            cell.posterView.image = nil
+        }
+        
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        cell.overviewLabel.sizeToFit()
+        
+        return cell
+    }
 
-    /*
+    
+
+   
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)
+        let movie = movies![(indexPath! as NSIndexPath).row]
+        
+        let detailViewController = segue.destination as! DetailViewController
+        detailViewController.movie = movie
+        
     }
-    */
-
+    
 }
