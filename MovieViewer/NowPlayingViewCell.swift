8e2fb757bf237d855d313818bb9fbc7692f62e12
Patwardhan, Saurabh <Saurabh.Patwardhan@capitalone.com>
2016-10-14 22:01:55 -0700

    Long overdue commit

---
+++ MovieViewer/NowPlayingViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NowPlayingViewCell.swift
+//  MovieViewer
+//
+//  Created by Patwardhan, Saurabh on 10/13/16.
+//  Copyright © 2016 Saurabh Patwardhan. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
